99c9491001ba3c7a5914b3344a696a9e5f235922
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-22 22:23:49 -0400

    Update 11_2.sql

--- 11_2.sql
+++ 11_2.sql
@@ -1,11 +1,4 @@
--- *** SqlDbx Personal Edition ***
--- !!! Not licensed for commercial use beyound 90 days evaluation period !!!
--- For version limitations please check http://www.sqldbx.com/personal_edition.htm
--- Number of queries executed: 6, number of rows retrieved: 15
-SELECT * FROM employee JOIN department ON department.emp_id = employee.emp_id WHERE department.Name='CSE';
-
-SELECT *FROM employee JOIN balance ON balance.emp_id = employee.emp_id;
-UPDATE department
+USE payroll;
 
 SELECT * FROM department JOIN employee ON employee.emp_id = department.emp_id
 WHERE department.Name='CSE';
@@ -32,16 +25,11 @@ SELECT employee.email FROM employee JOIN department ON department.emp_id = emplo
 
 SELECT time_off.Daysoff FROM employee JOIN time_off ON time_off.emp_id = employee.emp_id WHERE emp_name='Rohim';
 
--- fix this
-ALTER table employee ADD employee_phone VARCHAR(10);
--- SELECT * FROM employee;
--- ALTER TABLE employee DROP column employee_phone;
+ALTER table employee ADD column employee_phone VARCHAR(10);
 
 
-SELECT * FROM employee JOIN salary ON salary.emp_id = employee.emp_id WHERE base=8000
+SELECT * FROM employee JOIN salary ON salary.emp_id = employee.emp_id WHERE base=8000;
 
 SELECT employee.emp_name,max(balance.Bank_Balance) FROM employee JOIN balance ON balance.emp_id = employee.emp_id;
 
 SELECT employee.emp_name FROM employee JOIN balance ON balance.emp_id = employee.emp_id WHERE balance.Loan_Amount>0;
-
-SELECT * FROM employee WHERE emp_name='CSEMP07001';
